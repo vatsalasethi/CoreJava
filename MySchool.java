3de6d67d62983a172a163622cedd07149f3312f8
vatsalasethi <41329779+vatsalasethi@users.noreply.github.com>
2018-07-19 15:07:04 +0530

    Add files via upload

---
+++ MySchool.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class MySchool {
+	
+	
+	String nameOfTheSchool;
+	int noOfStudents;
+	String city;
+	
+
+}
---
+++ TestClass.java
@@ -0,0 +1,20 @@
+package pack2;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MySchool m=new MySchool();
+		m.city="New Delhi";
+		m.nameOfTheSchool="Manav Sthali School";
+		m.noOfStudents=86748;
+		
+		Trainer t=new Trainer();
+
+		LabTrainer l=new LabTrainer();
+		l.salary=62345;
+		
+		
+	}
+
+}
